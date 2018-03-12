8c34f534661968f322d6f29decd8d6a86e3e9cf3
honghui <252860883@qq.com>
2018-03-12 13:12:17 +0800

    添加antd

--- reactdemo/src/components/header.jsx
+++ reactdemo/src/components/header.jsx
@@ -5,10 +5,10 @@ export default class Header extends React.Component{
 
     render(){
         return(
-            <div class="header">
+            <div className="header">
                 <span>这是头部的信息</span>
             </div>
         )
     }
-
+    
 }
--- reactdemo/src/router/index.js
+++ reactdemo/src/router/index.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import { createHashHistory } from 'history';
 
 // 引入page
-import Foot from '../pages/footer';
-import Header from '../pages/header'
+import Foot from '../components/footer';
+import Header from '../components/header';
+
+import Page1 from '../pages/page1';
 
 
  const routes= (
     <BrowserRouter>
         <div>
             <Header/>
-            <Route path='/foot' component={Foot}></Route>
+            <Route path='/page1' component={Page1}></Route>
              {/* 公共组件写在这里 */}
             <Foot />
         </div>
